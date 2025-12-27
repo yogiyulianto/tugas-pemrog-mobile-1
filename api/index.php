92ed9e1e08bb3f4e3c19a8e3daadd6e7b6fa1bb4
Yogi Yulianto <34810088+yogiyulianto@users.noreply.github.com>
2025-12-27 15:36:07 +0700

    add index.php to api

---
+++ api/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php
+        // This is a single-line comment in PHP
+        echo '<p>Hello World</p>'; // The 'echo' statement outputs text
+    ?>
+</body>
+</html>
+
